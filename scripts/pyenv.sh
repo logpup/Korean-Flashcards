e8717d1daaa4674ab10b93cfe525ef66466cc10e
logpup <jyjprk@gmail.com>
2025-09-06 12:18:12 -0700

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	deleted:    notes/api_keys.txt
    	deleted:    notes/memo_sheet.txt
    	deleted:    scripts/pyenv.sh
    
    Changes not staged for commit:
    	modified:   .gitignore

--- scripts/pyenv.sh
+++
@@ -1,9 +0,0 @@
-# Replace 'my-project-env' with the name of your environment
-pyenv activate my-project-env
-
-# Install all third-party external dependencies at once
-pip install -r requirements.txt
-
-
-# Remember to deactivate the environment when you are done working with
-# pyenv deactivate
